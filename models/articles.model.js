2266706066aa4397fa7b5cddc7e8dd1b7ba6b0c2
yerayafonso <yafonso02@gmail.com>
2026-02-04 14:29:41 +0000

    amend fetchAllArticles query to a left join

--- models/articles.model.js
+++ models/articles.model.js
@@ -13,7 +13,7 @@ exports.fetchAllArticles = () => {
       articles.article_img_url, 
       COUNT(comments.article_id) AS comment_count 
       FROM articles 
-      JOIN comments 
+      LEFT JOIN comments 
       ON articles.article_id = comments.article_id 
       GROUP BY articles.article_id
       ORDER BY articles.created_at DESC;`,
