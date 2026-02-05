44f89d56744362dc9d260c368e6fb673a9a43d38
yerayafonso <yafonso02@gmail.com>
2026-02-05 14:30:11 +0000

    add a test for invalid method (delete) and the code to ensure the correct error handling

--- routes/articles.routes.js
+++ routes/articles.routes.js
@@ -12,6 +12,9 @@ articlesRouter.get("/", getAllArticles);
 
 articlesRouter.get("/:article_id", getArticleById);
 articlesRouter.get("/:article_id/comments", getArticleByIdComments);
+articlesRouter.delete("/:article_id", (req, res, next) => {
+  res.status(405).send({ msg: "Method not allowed" });
+});
 // articlesRouter.post("/:article_id/comments", postArticleByIdComments);
 
 module.exports = articlesRouter;
