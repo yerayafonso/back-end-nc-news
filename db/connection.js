e85d10c235ec3a66f414707bba687473684972ef
yerayafonso <yafonso02@gmail.com>
2026-02-06 23:25:18 +0000

    amend connection file to accept production env and throw errors appropriately

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,12 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-if (!process.env.PGDATABASE) {
+const config = {};
+const db = new Pool(config);
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+} else if (!process.env.PGDATABASE) {
   throw new Error("No PGDATABASE configured");
 } else {
   console.log(`Connected to ${process.env.PGDATABASE}`);
@@ -12,12 +17,6 @@ if (!process.env.PGDATABASE) {
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
-}
-const config = {};
-const db = new Pool(config);
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
 }
 
 module.exports = db;
