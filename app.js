73cde6deb005d2a10902e6a32b29a27d8ac743fe
yerayafonso <yafonso02@gmail.com>
2026-02-23 10:26:19 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const NotFoundError = require("./errors/NotFoundError");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static("public"));
 
