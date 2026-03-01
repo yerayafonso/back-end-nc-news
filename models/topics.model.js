2abb33497bd685ca2cdc271e1e265684438a0872
yerayafonso <yafonso02@gmail.com>
2026-03-01 18:48:39 +0000

    update topic model to return img url

--- models/topics.model.js
+++ models/topics.model.js
@@ -2,12 +2,6 @@ const db = require("../db/connection");
 
 exports.fetchAllTopics = () => {
   return db
-    .query(`SELECT slug, description FROM topics`)
-    .then(({ rows }) => rows);
-};
-
-exports.fetchAllArticlesByTopic = (topic) => {
-  return db
-    .query(`SELECT * FROM articles WHERE topic = $1`, [topic])
+    .query(`SELECT slug, description, img_url FROM topics`)
     .then(({ rows }) => rows);
 };
